204d67f7dad943b22d079e662b3f09a3e65a2bc5
Serzh0990 <serzh0990@gmail.com>
2013-02-23 12:40:10 +0200

    static pages complete

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+    it "should_have_a_title" do
+      get 'home'
+      response.should have_selector("title",
+                      :content=>" | Home")
     end
   end
 
@@ -13,6 +19,20 @@ describe PagesController do
     it "returns http success" do
       get 'contact'
       response.should be_success
+    end
+    it "should_have_a_title" do
+      response.should have_selector("title",
+                      :content=>" | Contact")
+    end
+  end
+  describe "GET 'about'" do
+    it "returns_http_success" do
+      get 'about'
+      response.should be_success
+    end
+    it "should_have_a_title" do
+      response.should have_selector("title",
+                      :content=>" | About")
     end
   end
 
