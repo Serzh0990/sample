e049a7e2724c20fab26aa4f5157ff2941cbb7c64
Serzh0990 <serzh0990@gmail.com>
2013-03-05 21:32:14 +0200

    users login and out success. go to chapter 10 (cherry picked from commit 24c1142)

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -93,4 +93,24 @@ describe UsersController do
     end
   end
 
+  describe "GET'edit'" do
+    before(:each)do
+      @user=Factory(:user)
+      test_sign_in(@user)
+    end
+    it "should be successful" do
+      get :edit, id: @user
+      response.should be_success
+    end
+     it "should have right title" do
+       get :edit, id: @user
+       response.should have_selector("title" , content: "Edit user")
+     end
+    it "should have a link to change gravatar" do
+      get :edit, id: @user
+      gravatar_url = "http://gravatar.com/emails"
+      response.should have_selector("a", href: gravatar_url,content:"Change Avatar")
+    end
+  end
+
 end
